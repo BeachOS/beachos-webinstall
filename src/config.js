1f3bbefa80ac00d0377cdce75bb52bab321bcef1
root <Beach OS>
2023-01-11 08:29:08 +0000

    BeachOS

---
+++ src/config.js
@@ -0,0 +1,69 @@
+export default {
+    OS_NAME: process.env.VUE_APP_OS_NAME,
+    ACCENT_COLOR: "#0366d6",
+    SUPPORTED_DEVICES: [
+        {
+            name: "Pixel 2",
+            model: "walleye",
+        },
+        {
+            name: "Pixel 2 XL",
+            model: "taimen",
+        },
+        {
+            name: "Pixel 3",
+            model: "blueline"
+        },
+        {
+            name: "Pixel 3 XL",
+            model: "crosshatch"
+        },
+        {
+            name: "Pixel 3a",
+            model: "sargo"
+        },
+        {
+            name: "Pixel 3a XL",
+            model: "bonito"
+        },
+        {
+            name: "Pixel 4",
+            model: "flame",
+        },
+        {
+            name: "Pixel 4 XL",
+            model: "coral",
+        },
+        {
+            name: "Pixel 4a",
+            model: "sunfish"
+        },
+        {
+            name: "Pixel 5",
+            model: "redfin",
+        },
+        {
+            name: "Pixel 4a (5G)",
+            model: "bramble",
+        },
+        {
+            name: "Pixel 5a (5G)",
+            model: "barbet",
+        },
+    ],
+    RELEASE_VARIANTS: {
+        stable: {
+            description: "Stable release.",
+            suffix: "",
+        },
+    },
+    DONATION_LINKS: [
+        {
+            title: "Make a Tax-deductible Donation to Beach Institute",
+            description: "You can donate through paypal, cryptocurrency, or by mail.",
+            highlight: false,
+            url: "https://members.beachinstitute.org/donate",
+            icon: "",
+        },
+    ],
+};
